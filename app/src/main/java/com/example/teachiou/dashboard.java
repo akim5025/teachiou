0b6721045bfb211a1e15be79c28194ae184db1a0
akim5025 <75850967+akim5025@users.noreply.github.com>
2022-03-10 08:19:32 -0600

    fragments (broken)

---
+++ app/src/main/java/com/example/teachiou/dashboard.java
@@ -0,0 +1,49 @@
+package com.example.teachiou;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class dashboard extends AppCompatActivity {
+
+    //Coding in Flow: "BottomNavigationView with Fragments - Android Studio Tutorial"
+    //https://www.youtube.com/watch?v=tPV8xA7m-iw&t=185s
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener  navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = new FragmentHome();
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new FragmentHome();
+                            break;
+                        case R.id.nav_notification:
+                            selectedFragment = new FragmentNotifications();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new FragmentProfile();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
