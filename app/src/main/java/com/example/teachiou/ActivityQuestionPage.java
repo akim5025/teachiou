abe37909223638997fb56a9753d8b1ae252a6f9a
KevinZhou168 <84091414+KevinZhou168@users.noreply.github.com>
2022-03-10 08:08:14 -0600

    created java file for questions page

---
+++ app/src/main/java/com/example/teachiou/ActivityQuestionPage.java
@@ -0,0 +1,18 @@
+package com.example.teachiou;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityQuestionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_page);
+    }
+
+
+
+}
