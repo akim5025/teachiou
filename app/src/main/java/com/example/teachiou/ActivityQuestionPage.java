6dbc332bbacf8969ed09c4aa906cedcf1933cb19
KevinZhou168 <84091414+KevinZhou168@users.noreply.github.com>
2022-03-14 07:55:27 -0500

    Created ActivityQuestionPage and set up ViewQuestion class

--- app/src/main/java/com/example/teachiou/ActivityQuestionPage.java
+++ app/src/main/java/com/example/teachiou/ActivityQuestionPage.java
@@ -1,7 +1,12 @@
 package com.example.teachiou;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +21,26 @@ public class ActivityQuestionPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_page);
+
+        myList = MainActivity.firebaseHelper.getWishListItems();
+        Intent intent = getIntent();
+
+        ArrayAdapter<Question> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, myList);
+
+        ListView listView = (ListView) findViewById(R.id.questionList);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ActivityQuestionPage.this, ViewQuestion.class);
+
+                intent.putExtra("QUESTION", myList.get(i));
+                startActivity(intent);
+
+            }
+        });
     }
 
 
