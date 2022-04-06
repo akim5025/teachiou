ff537f2e043b94da98cfd28901b1c065f5de70eb
willKChen24 <84326582+willKChen24@users.noreply.github.com>
2022-04-06 08:16:58 -0500

    finished loading screen (still needs to be implemented)

--- app/src/main/java/com/example/teachiou/loadScreenActivity.java
+++ app/src/main/java/com/example/teachiou/loadScreenActivity.java
@@ -3,12 +3,36 @@ package com.example.teachiou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class loadScreenActivity extends AppCompatActivity {
+
+    private boolean switchOn= false;
+
+    LottieAnimationView lottieLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
+
+        lottieLoad = findViewById(R.id.lottieLoad);
+
+        lottieLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(switchOn){
+                    lottieLoad.setMinAndMaxProgress(0.5f, 1.0f);
+                    lottieLoad.playAnimation();
+                    switchOn = false;
+                }else{
+                    lottieLoad.setMinAndMaxProgress(0.0f, 0.5f);
+                    lottieLoad.playAnimation();
+                    switchOn = true;
+                }
+            }
+        });
     }
 }
