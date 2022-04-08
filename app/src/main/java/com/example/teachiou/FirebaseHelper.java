d5e4829fbfa52748360dcee868038678a77c09fd
KevinZhou168 <84091414+KevinZhou168@users.noreply.github.com>
2022-04-08 08:05:16 -0500

    import SetOptions

--- app/src/main/java/com/example/teachiou/FirebaseHelper.java
+++ app/src/main/java/com/example/teachiou/FirebaseHelper.java
@@ -25,6 +25,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -155,7 +156,7 @@ public class FirebaseHelper {
                     }
                 });
     }
-
+    // .set(q, SetOptions.merge()) does not overwrite data
     private void addData(Question q, String className, FirestoreCallback firestoreCallback) {
         db.collection("classes").document(className).collection("questions")
                 .add(q)
